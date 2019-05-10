15130d986f139c1366f79b8498d6c2609950ed3f
DrFreud1 <38196801+DrFreud1@users.noreply.github.com>
2019-05-09 22:18:06 -0500

    Add files via upload

--- matrix_operations.h
+++ matrix_operations.h
@@ -2,6 +2,21 @@
 
 //          Matrix operations routines
 //---------------------------------------------------------------------------
+void print_matrix(char matrix[rows][cols])
+{
+ int i,j;
+    
+        for(i=0;i<rows;i++)    // matrix [fila][column]
+            {
+            cout<<endl;
+            for (j=0;j<cols;j++)
+                {
+                 cout<<" " <<matrix[i][j];  // mantiene fija la fila y barre las columnas 
+                }
+            }
+        cout<<endl<<"---------"<<endl;
+}
+
 void print_aux_matrix(void)
 {
  int i,j;
@@ -257,14 +272,44 @@ void ShiftRows(void) {
   void ModuloMultiplication(int vector[4]) {
     //codigo de la multiplicacion mod 16
   }
-
-  void MixColumns(void) {
-    int arr[4];
-    for (size_t j = 0; j < 4; j++) {
-      for (size_t i = 0; i < 4; i++) {
-        arr[i]=state_hex_matrix[i][j];
-      }
-      ModuloMultiplication(arr);
-    }
-
+  
+  void matrixToArray(void){
+      int k = 0;
+      for(int i = 0; i<rows ; i++)
+          for(int j = 0; j<cols; j++){
+              state[k++] = state_hex_matrix[i][j];
   }
+  }
+  
+
+  void MixColumns(unsigned char * state) {
+	unsigned char tmp[16];
+
+	tmp[0] = (unsigned char) mul2[state[0]] ^ mul3[state[1]] ^ state[2] ^ state[3];
+	tmp[1] = (unsigned char) state[0] ^ mul2[state[1]] ^ mul3[state[2]] ^ state[3];
+	tmp[2] = (unsigned char) state[0] ^ state[1] ^ mul2[state[2]] ^ mul3[state[3]];
+	tmp[3] = (unsigned char) mul3[state[0]] ^ state[1] ^ state[2] ^ mul2[state[3]];
+
+	tmp[4] = (unsigned char)mul2[state[4]] ^ mul3[state[5]] ^ state[6] ^ state[7];
+	tmp[5] = (unsigned char)state[4] ^ mul2[state[5]] ^ mul3[state[6]] ^ state[7];
+	tmp[6] = (unsigned char)state[4] ^ state[5] ^ mul2[state[6]] ^ mul3[state[7]];
+	tmp[7] = (unsigned char)mul3[state[4]] ^ state[5] ^ state[6] ^ mul2[state[7]];
+
+	tmp[8] = (unsigned char)mul2[state[8]] ^ mul3[state[9]] ^ state[10] ^ state[11];
+	tmp[9] = (unsigned char)state[8] ^ mul2[state[9]] ^ mul3[state[10]] ^ state[11];
+	tmp[10] = (unsigned char)state[8] ^ state[9] ^ mul2[state[10]] ^ mul3[state[11]];
+	tmp[11] = (unsigned char)mul3[state[8]] ^ state[9] ^ state[10] ^ mul2[state[11]];
+
+	tmp[12] = (unsigned char)mul2[state[12]] ^ mul3[state[13]] ^ state[14] ^ state[15];
+	tmp[13] = (unsigned char)state[12] ^ mul2[state[13]] ^ mul3[state[14]] ^ state[15];
+	tmp[14] = (unsigned char)state[12] ^ state[13] ^ mul2[state[14]] ^ mul3[state[15]];
+	tmp[15] = (unsigned char)mul3[state[12]] ^ state[13] ^ state[14] ^ mul2[state[15]];
+
+	for (int i = 0; i < 16; i++) {
+		state[i] = tmp[i];
+	}
+    
+    puts("MixColums: ");
+    for(int i = 0; i<16;i++){
+        printf("%x ", state[i]);}
+}
